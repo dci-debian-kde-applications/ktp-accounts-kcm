c51f1471f9f044b56393772a016ad556e0a51037
Diane Trout <diane@ghic.org>
2014-09-21 11:03:10 -0700

    Imported Upstream version 0.8.80

---
+++ plugins/morse/ktpaccountskcm_plugin_morse.desktop.cmake
@@ -0,0 +1,14 @@
+[Desktop Entry]
+Encoding=UTF-8
+Comment=Custom account configuration user interface for accounts using the Morse connection manager.
+Type=Service
+ServiceTypes=KTpAccountsKCM/AccountUiPlugin
+
+X-KDE-Library=ktpaccountskcm_plugin_morse
+X-KDE-PluginInfo-Author=David Edmundson
+X-KDE-PluginInfo-Email=kde@davidedmundson.co.uk
+X-KDE-PluginInfo-Name=morse
+X-KDE-PluginInfo-Version=@KTP_ACCOUNTS_KCM_VERSION@
+X-KDE-PluginInfo-Website=http://techbase.kde.org/Projects/Telepathy
+X-KDE-PluginInfo-License=GPL
+X-KDE-PluginInfo-EnabledByDefault=true
